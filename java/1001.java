0dc50fead4e723f63b8a380def247fa099560510
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2022-03-16 21:26:37 -0300

    Create 1001.java

---
+++ java/1001.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a, b, soma;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        soma = a + b;
+
+        System.out.println("X = " + soma);
+        
+        sc.close();
+    }
+ 
+}
